e55202e48e9ffe4904967d93e375fc96becc07fd
ManifoldHydra80 <91525267+ManifoldHydra80@users.noreply.github.com>
2023-10-08 15:36:58 +1100

    Added my project

---
+++ collection/1.js
@@ -0,0 +1,8 @@
+Content=['https://www.youtube.com/watch?v=q6-ZGAGcJrk',
+'https://www.youtube.com/watch?v=ffB5ZwJ41AM&t=5s',
+'https://www.youtube.com/watch?v=azhq06zD8Hk',
+'https://www.youtube.com/watch?v=CJavvurdoII&t=15s',
+'https://www.tiktok.com/@starkrafingmad/video/6887706324042403078',
+'https://www.youtube.com/watch?v=ZVuToMilP0A',
+'https://www.youtube.com/watch?v=NWi5yA9LhB4'
+];
